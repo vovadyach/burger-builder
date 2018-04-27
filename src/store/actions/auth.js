f32167abb78aecc46bb7601523f78e65c294e7ce
vvd <vvd@norse.digital>
2018-04-27 15:37:26 +0300

    Adds the firebase token for auth.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 
 export const authStart = () => {
@@ -24,5 +25,18 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyChNM9O0i1X-5X2xPNl7pLd2F3t8dUxm1k', authData)
+            .then(res => {
+                dispatch(authSuccess(res.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err)); 
+            })
     };
 };
